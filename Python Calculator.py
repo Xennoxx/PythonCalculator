9fb1d6e7bfc402ceadb972f205cce6a68ff4531f
Xennoxx <nicknifffe@gmail.com>
2023-03-06 00:37:27 +0100

    Attempt 3

--- Python Calculator.py
+++ Python Calculator.py
@@ -1,5 +1,5 @@
-operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the 1st number: "))
+operator = input("Enter an operator like... (+ - * /): ")
 num2 = float(input("Enter the 2nd number: "))
 
 
@@ -9,6 +9,9 @@ elif operator == "-":
     result = num1 - num2
 elif operator == "*":
     result = num1 * num2
+elif num2 == 0:
+    print("Thats satanic")
+    exit()
 elif operator == "/":
     result = num1 / num2
 else:
